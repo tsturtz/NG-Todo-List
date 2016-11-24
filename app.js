ae038b39ea402f72bf9632bfbaa2f5ad4287bbec
Taylor Sturtz <taylorsturtz.com>
2016-11-23 19:46:27 -0800

    Angular Material UI mostly done

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 angular.module('todoApp', ['ngMaterial'])
 
-.controller('formCtrl', function() {
+.config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('amber')
+        .accentPalette('indigo')
+        .warnPalette('red')
+        .dark();
+})
 
+.controller('loginCtrl', function() {
+
+})
+
+.controller('taskCtrl', function () {
+    var taskSelf = this;
+
+    taskSelf.addTask = function () {
+        console.log ('add task clicked')
+    };
+
+/*    taskSelf.tasks = [
+        item: {
+        task: 'kill kevin'
+        }
+    ];*/
 });
