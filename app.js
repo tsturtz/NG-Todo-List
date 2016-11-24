26727e1db8f49a7e768393c15de7471aaddbff7f
Taylor Sturtz <taylorsturtz.com>
2016-11-24 00:55:35 -0800

    2 steps forward, 1.999999 steps back

--- app.js
+++ app.js
@@ -1,27 +1,48 @@
 angular.module('todoApp', ['ngMaterial'])
 
-.config(function($mdThemingProvider) {
-    $mdThemingProvider.theme('default')
-        .primaryPalette('amber')
-        .accentPalette('indigo')
-        .warnPalette('red')
-        .dark();
-})
+    .config(function ($mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('amber')
+            .accentPalette('grey')
+            .warnPalette('red')
+            .dark();
+    })
 
-.controller('loginCtrl', function() {
+    .controller('loginCtrl', function () {
 
-})
+    })
 
-.controller('taskCtrl', function () {
-    var taskSelf = this;
+    .controller('taskCtrl', function () {
+        var taskSelf = this;
 
-    taskSelf.addTask = function () {
-        console.log ('add task clicked')
-    };
+        taskSelf.editTaask = function () {
+            console.log('edit task clicked');
+        };
 
-/*    taskSelf.tasks = [
-        item: {
-        task: 'kill kevin'
-        }
-    ];*/
-});
+        taskSelf.addTask = function () {
+            console.log('add task clicked');
+        };
+
+        taskSelf.deleteTask = function () {
+            console.log('delete task clicked');
+        };
+
+        taskSelf.todos = [
+            {
+                task: 'walk the carpet',
+                date: '11/23/16'
+            },
+            {
+                task: 'clean the trash',
+                date: '11/24/16'
+            },
+            {
+                task: 'vacuum the dog',
+                date: '11/25/16'
+            },
+            {
+                task: 'take out the house',
+                date: '11/26/16'
+            }
+        ];
+    });
