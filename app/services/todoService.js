a65194af5113705b647d8885cea96b17db228c11
taylor_sturtz <taylorsturtz@gmail.com>
2017-01-19 12:02:39 -0800

    some layout changes. beginning to set up firebaseAuth

--- app/services/todoService.js
+++ app/services/todoService.js
@@ -1,12 +1,8 @@
 angular.module('todoApp')
 
-    .service('todoService', function ($firebaseArray, $q) {
+    .service('todoService', function ($firebaseArray, $firebaseAuth, $q) {
 
-        /**
-         * Initialize Firebase
-         * @type {{apiKey: string, authDomain: string, databaseURL: string, storageBucket: string, messagingSenderId: string}}
-         */
-
+        // Initialize Firebase
         var config = {
             apiKey: "AIzaSyCEGQoW8RUnkoTiyFFkzOmdTIk54GpTup8",
             authDomain: "todoappdb.firebaseapp.com",
@@ -22,17 +18,6 @@ angular.module('todoApp')
         svc.getTodos = function(where){
             var ref = firebase.database().ref().child(where);
             return $firebaseArray(ref);
-        };
-
-        svc.addTodo = function (todo) {
-            var defer = $q.defer();
-            console.log('todo passed into service: ', todo);
-            fb.ref('todos').once('value', function(snapshot) {
-                console.log('snapshot from service: ', snapshot.val());
-                fb.ref('todos/' + snapshot.val().length).set(todo);
-                defer.resolve(snapshot.val());
-            });
-            return defer.promise;
         };
 
     });
