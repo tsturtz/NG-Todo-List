9073904f8d50b47719a56268068a9cdb7590fb97
taylor_sturtz <taylorsturtz@gmail.com>
2017-01-20 17:52:28 -0800

    minor edits

--- app/controllers/rootCtrl.js
+++ app/controllers/rootCtrl.js
@@ -185,17 +185,6 @@ angular.module('todoApp')
             $mdToast.hide();
         };
 
-        /*//Initiate spinner
-        self.activated = true;
-
-        //Bind todos array to Firebase through AngularFire
-        self.todos = todoService.getTodos('todos');
-
-        //Turn off spinner after data is initially loaded
-        self.todos.$loaded(function (){
-            self.activated = false;
-        });*/
-
         //Toggle the side menu
         self.openLeftMenu = function () {
             $mdSidenav('left').toggle();
